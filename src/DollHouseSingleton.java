e9124c6dee406482f3d3eebf4755f6b0a93d40c0
Meblom <xuniqh@gmail.com>
2018-03-07 15:01:52 +0100

    sedfsef

--- src/DollHouseSingleton.java
+++ src/DollHouseSingleton.java
@@ -7,10 +7,10 @@ public class DollHouseSingleton {
     this.navn = "Dukkehuset i Århus";
   }
 
-  public  DollHouseSingleton getInstance() {
-    if (this.instance==null) {
+  public static DollHouseSingleton getInstance() {
+    if (instance==null) {
       DollHouseSingleton instance = new DollHouseSingleton();
     }
-  return this.instance;
+  return instance;
   }
 }
